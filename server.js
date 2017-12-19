8dcb5d09868bffe7c3513472ca1f0614196d427f
Yann <34681489+saldoyann@users.noreply.github.com>
2017-12-19 17:06:08 +0100

    Update server.js
    update app.listen et création d'un app.get

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.get('/login', function(req, res){
     res.render('login');
 });
 
-app.listen(1313);
+app.get('/ping', function(req, res){
+    res.send('ping');
+});
+
+app.listen(process.env.PORT || 1313);
 
 
 
