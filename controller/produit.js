7d917d0d9c060016c5eb07d2c77ab6b8e19b85f5
SALDO Yann <sy501613@iutnice.unice.fr>
2018-01-15 13:22:43 +0100

    résolution beug ajout de produit

--- controller/produit.js
+++ controller/produit.js
@@ -1,9 +1,12 @@
 var Produit = require('../modele/produit.js');
 
 module.exports.ajout = function(req,res){
-
-  Produit.create({ nomProduit: req.body.nomProduit, prixProduit: req.body.prixProduit, quantiteProduit: req.body.quantiteProduit })
-  .then(produit => {
-    res.render('/panneauAdmin');
+	console.log(req.body);
+  Produit.create({
+  	nomProduit: req.body.nomProduit, 
+  	prixProduit: req.body.prixProduit, 
+  	quantiteProduit: req.body.quantiteProduit 
+  }).then(produit => {
+    res.render('panneauAdmin');
   })
 }
